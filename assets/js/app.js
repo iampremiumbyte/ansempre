b53a3b18586e4d5b632fab054baa3880da33a086
Godwin Idemudia <godwinide@gmail.com>
2024-05-10 22:02:09 +0100

    new

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+
+const enter = document.getElementById("enter");
+const overlay = document.getElementById("overlay");
+const audio = document.getElementById("audio");
+
+
+enter.addEventListener("click", () => {
+    overlay.style.display = "none";
+    audio.play();
+})
+
+
+const styleBoxes = () => {
+
+    Array(4).fill(0).forEach((_, i) => {
+        const box = document.getElementById(`box${i + 1}`);
+
+        const area = document.getElementById(`area${i + 1}`);
+
+        const coords = area.coords.split(",");
+
+        box.style.width = (Math.abs(coords[0] - coords[2])) + "px"
+        box.style.height = (Math.abs(coords[1] - coords[3])) + "px"
+        box.style.left = Number(coords[0]) + "px"
+        box.style.top = Number(coords[1]) + "px"
+    });
+
+}
+
+setInterval(() => {
+    styleBoxes();
+}, 2000)
+
